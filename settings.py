df0109ebf8b78c3a92eb139abb71fc67313f986a
Владимир Назаров <vova24848@gmail.com>
2021-12-17 18:41:58 +0300

    set .text

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # Settings
-name_section_of_riscv = '.text.init'  # '.text.init'
+name_section_of_riscv = '.text'  # '.text.init'
 
 # Out formats
 symtab_name = "{} {:15} {:>5} {:8} {:8} {:8} {:>6} {}\n".format(
